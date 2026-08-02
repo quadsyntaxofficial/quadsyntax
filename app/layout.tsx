55fd398eaeaa0f312318db8b23656541b60e855c
Saqalain Abid <saqalainabid1@gmail.com>
2026-08-02 14:38:21 +0500

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <GlobalLoader />
+        {/* <GlobalLoader /> */}
         {children}
       </body>
     </html>
